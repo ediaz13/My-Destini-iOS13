819c45e0c24c380b59ce9808a8684b41292a3e21
ediaz13 <bbadmin@MBP-de-BBADmin.fibertel.com.ar>
2024-04-11 13:01:25 -0300

    Make the StoryBrain more Scalable

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -9,15 +9,18 @@
 import Foundation
 
 struct Story {
-    let story0: String
+    let title: String
     let choice1: String
+    let choice1Destination: Int
     let choice2: String
+    let choice2Destination: Int
     
-    init(story0: String, choice1: String, choice2: String) {
-        self.story0 = story0
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
+        self.title = title
         self.choice1 = choice1
+        self.choice1Destination = choice1Destination
         self.choice2 = choice2
+        self.choice2Destination = choice2Destination
     }
-    
     
 }
